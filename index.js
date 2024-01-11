ac0f9722611d04ca36bfe5c1e5d139228a907949
Vincent Bazia <vincent.bazia@gmail.com>
2024-01-11 16:40:13 +0100

    Parfait

--- index.js
+++ index.js
@@ -1,40 +1,60 @@
-const { WebSocketServer } = require("ws")
-
+const ws = require("ws")
+const express = require("express")
 const crypto = require("crypto")
+
+const app = express()
 
 const PORT = process.env.PORT || 4321
 
-const wss = new WebSocketServer({ port: PORT })
+let users = []
 
-console.log("SERVER ON ", PORT)
-
-let clients = []
-
+const wss = new ws.Server({ noServer: true })
 wss.on("connection", ws => {
-    let id = crypto.randomBytes(4).toString("hex")
+    let id = crypto.randomUUID()
     ws.userId = id
+    ws.metadata = {}
+
+    users.push(ws)
 
     ws.send(JSON.stringify({ type: "id", data: ws.userId }))
 
-    clients.push(ws)
-
-    ws.on("error", err => {
-        console.error("error", err)
-    })
+    ws.on("error", err => console.error("error", err))
 
     ws.on("message", message => {
         try {
             let { type, data } = JSON.parse(message)
 
             switch (type) {
+                case "id":
+                    if (!users.find(c => c.userId == data)) {
+                        ws.userId = data
+                        ws.send(JSON.stringify({ type: "id", data: ws.userId }))
+                    }
+                    break
                 case "peer":
-                    clients.find(c => c.userId == data.id).send(JSON.stringify({ type: "peer", data: { ...data, id: ws.userId } }))
+                    users.find(c => c.userId == data.id).send(JSON.stringify({ type: "peer", data: { ...data, id: ws.userId } }))
+                    break
+
+                case "data":
+                    ws.metadata = data
                     break
             }
         } catch (error) {}
     })
 
-    ws.on("close", () => {
-        clients = clients.filter(c => c.userId != ws.userId)
+    ws.on("close", () => (users = users.filter(c => c.userId != ws.userId)))
+})
+
+app.get("/", (req, res) => {
+    const filters = req.query
+    res.json(users.map(user => ({ id: user.userId, data: user.metadata })))
+})
+
+const server = app.listen(PORT)
+server.on("upgrade", (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, socket => {
+        wss.emit("connection", socket, request)
     })
 })
+
+console.log("SERVER ON ", PORT)
