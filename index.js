1dffa100a617c6ffa2c5b576eb45ddaebc5828a8
Vincent Bazia <vincent.bazia@gmail.com>
2024-07-01 19:33:36 +0200

    nickel

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const crypto = require("crypto")
 
 const app = express()
 
-const PORT = process.env.PORT || 4321
+const PORT = process.env.PORT || 3000
 
 let users = []
 
 const wss = new ws.Server({ noServer: true })
+
 wss.on("connection", ws => {
     let id = crypto.randomUUID()
     ws.userId = id
-    ws.metadata = {}
 
     users.push(ws)
 
@@ -34,10 +34,6 @@ wss.on("connection", ws => {
                 case "peer":
                     users.find(c => c.userId == data.id).send(JSON.stringify({ type: "peer", data: { ...data, id: ws.userId } }))
                     break
-
-                case "data":
-                    ws.metadata = data
-                    break
             }
         } catch (error) {}
     })
@@ -45,16 +41,7 @@ wss.on("connection", ws => {
     ws.on("close", () => (users = users.filter(c => c.userId != ws.userId)))
 })
 
-app.get("/", (req, res) => {
-    const filters = req.query
-    res.json(users.map(user => ({ id: user.userId, data: user.metadata })))
-})
-
 const server = app.listen(PORT)
-server.on("upgrade", (request, socket, head) => {
-    wss.handleUpgrade(request, socket, head, socket => {
-        wss.emit("connection", socket, request)
-    })
-})
-
 console.log("SERVER ON ", PORT)
+
+server.on("upgrade", (request, socket, head) => wss.handleUpgrade(request, socket, head, socket => wss.emit("connection", socket, request)))
